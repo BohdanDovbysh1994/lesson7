a5383c0084ac7659911c54679dcae8588cba7387
DovbyshBohdan <dovbysh.bohdan@gmail.com>
2017-02-20 21:09:10 +0200

    init comit

---
+++ Lesson7/src/Main/Main.java
@@ -0,0 +1,24 @@
+package Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String [] fraction = {"freedom", "independence", "green"};
+		
+		for (int i = 0; i < fraction.length; i++) {
+			System.out.println(fraction[i]);
+		}
+		
+		System.out.println();
+		
+		for(String n : fraction){
+			System.out.println(n);
+		}
+		
+		
+		
+	}
+
+}
