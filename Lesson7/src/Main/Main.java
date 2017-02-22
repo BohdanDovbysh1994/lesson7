e62e80b5f418aefe4842ac6a5c6c76ebcdc3bb3e
bohdan <dovbysh.bohdan@gmail.com>
2017-02-22 13:54:03 +0200

    Update Main.java

--- Lesson7/src/Main/Main.java
+++ Lesson7/src/Main/Main.java
@@ -5,33 +5,40 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String [] fraction = {"freedom", "independence", "green"};
+// 		String [] fraction = {"freedom", "independence", "green"};
 		
-		for (int i = 0; i < fraction.length; i++) {
-			System.out.println(fraction[i]);
-		}
+// 		for (int i = 0; i < fraction.length; i++) {
+// 			System.out.println(fraction[i]);
+// 		}
 		
-		System.out.println();
+// 		System.out.println();
 		
-		for(String n : fraction){
-			System.out.println(n);
-		}
+// 		for(String n : fraction){
+// 			System.out.println(n);
+// 		}
 		
-		System.out.println("From git");
-		int balance = 5000;
-		
+// 		System.out.println("From git");
 		Scanner scanner = new scanner(Scanner.in);
-		
-		System.out.println("Enter 1 for cheking balance");
-		System.out.println("Enter 2 for credit");
-		System.out.println("Enter 3 for get money");
-		System.out.println("Enter 4 for exit");
+		int balance = 5000;
 		boolean bankomat = true;
-		int scanner = scanner.NextInt();
+		String login1 = Bohdan;
+		String login = scanner.Next;
+		int password1 = 1111;
+		int password = scanner.NextInt;
+		
+		System.out.println ("Enter your login)
+		if (login equals.login1){
+		System.out.println("Enter your password")
+			if (passwor == password1){
+			System.out.println("Enter 1 for cheking balance");
+			System.out.println("Enter 2 for credit");
+			System.out.println("Enter 3 for get money");
+			System.out.println("Enter 4 for exit");
+		int key = scanner.NextInt();
 		while (bankomat)
 		switch (key){
 			case "1":{
-			System.out.println("balance");
+			System.out.println(balance);
 				break;
 			}
 			case "2":{
@@ -39,23 +46,29 @@ public class Main {
 				System.out.println("How much money you want get?");
 				int credit = scanner.NextInt();
 				balance = balance - credit;
+				System.out.println("Your balance is" + balance);
 			} else { System.out.println("no money");
 			}
 				break;
 			}
 			case "3":{
-				System.out.println("How much money you wan.....?");
+				System.out.println("How much money you want.....?");
 				int money = scanner.NextInt();
 				balance = balance + money;
+				System.out.println("Your balance is" + balance);
 				break;
 			}
 			case "4":{
 				bankomat = false;
-				exit;
+				System.exit(4);
 				break;
 			}	
+		default {
+				System.out.println("Wrong comand")
 		}
-		
+		}
+		} else {System.out.println ("Inkorect login")}
+		} else {System.out.println ("Incorekt password")}
 	}
 
 }
